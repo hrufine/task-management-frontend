95cdcdd90088dee8dc569b239acb63df58447daf
hrufine <hrufine@users.noreply.github.com>
2021-08-03 13:37:39 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.1f6751596f0e62d6dff05a8e3c89e4c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b53f9b94be224339e4d95d2f0402bb7",
+    "url": "https://hrufine.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "6027c2e8f2aad2c0e763",
+    "url": "https://hrufine.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "1e5a81906d3932c44bda",
+    "url": "https://hrufine.github.io/task-management-frontend/static/js/2.7f718189.chunk.js"
+  },
+  {
+    "revision": "6027c2e8f2aad2c0e763",
+    "url": "https://hrufine.github.io/task-management-frontend/static/js/main.356d5f94.chunk.js"
+  },
+  {
+    "revision": "415cda4386c73e855497",
+    "url": "https://hrufine.github.io/task-management-frontend/static/js/runtime~main.8e531170.js"
+  }
+]);
